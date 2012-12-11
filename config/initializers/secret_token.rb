05e0ccc05143788d5cd3804efa5d729d152ab9b5
waitingkuo <waitingkuo0527@gmail.com>
2012-12-11 23:35:32 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EasyAds::Application.config.secret_token = '195ad13cfa1dfe200bef6c5249254ed14d7a89fcd802d382843763bd6463f699e39ce4c46cdb20790fb6818381bf9e7c7a53020ce9377c778fdd1484649de547'
